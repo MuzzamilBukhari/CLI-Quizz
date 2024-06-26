ba8e9391576cb13cce0f0e6d4ce799a86f251a8a
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-06-26 09:56:04 +0500

    fixing errors:

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 #! /usr/bin/env node
+import inquirer from "inquirer";
 // initialize questions array
 let questions = [];
 // Adding quiz questions to array
 // q 1
-let question1 = {
-    question: "What is inquirer?",
+questions.push({
+    question: "Q no 1 ) What is inquirer?",
     options: ["Package", "Software", "Program", "Application"],
     correctAnsIndex: 0,
-};
-questions.push(question1);
+});
 // q 2
-let question2 = {
-    question: "What is the actual name of arrow function?",
+questions.push({
+    question: "Q no 2 ) What is the actual name of arrow function?",
     options: [
         "Async function",
         "Normal FUnction",
@@ -19,19 +19,40 @@ let question2 = {
         "None of these",
     ],
     correctAnsIndex: 2,
-};
-questions.push(question2);
+});
 // q 3
-let question3 = {
-    question: "What is the output of console.log( 2 + '3' ) ?",
+questions.push({
+    question: "Q no 3 ) What is the output of console.log( 2 + '3' ) ?",
     options: ["5", "23", "Error", "None of these"],
     correctAnsIndex: 0,
-};
-questions.push(question3);
+});
 // console.log(questions);
 // I add 3 questions, you can add more as you want...
 // Now starting the quiz
 console.log("-".repeat(50));
 console.log("\tWelcome in Typescript Quizz...");
 console.log("-".repeat(50));
-export {};
+let result = 0;
+for (let i = 0; i < questions.length; i++) {
+    console.log(questions[i].question);
+    let answer = await inquirer.prompt([
+        {
+            type: "list",
+            name: "option",
+            choices: questions[i].options,
+            message: "Select an option : ",
+        },
+    ]);
+    if (answer.option === questions[i].options[questions[i].correctAnsIndex]) {
+        result++;
+        console.log("Correct");
+    }
+    else {
+        console.log("\n\tIncorrect answer! correct answer is " +
+            questions[i].options[questions[i].correctAnsIndex] +
+            "\n");
+    }
+}
+console.log(`You give ${result} out of ${questions.length} correct answers.`);
+let percentage = (result / questions.length) * 100;
+console.log(`Your percentage is ${percentage}`);
--- index.ts
+++ index.ts
@@ -12,16 +12,15 @@ let questions: Question[] = [];
 
 // Adding quiz questions to array
 // q 1
-let question1: Question = {
-  question: "What is inquirer?",
+questions.push({
+  question: "Q no 1 ) What is inquirer?",
   options: ["Package", "Software", "Program", "Application"],
   correctAnsIndex: 0,
-};
-questions.push(question1);
+});
 
 // q 2
-let question2: Question = {
-  question: "What is the actual name of arrow function?",
+questions.push({
+  question: "Q no 2 ) What is the actual name of arrow function?",
   options: [
     "Async function",
     "Normal FUnction",
@@ -29,16 +28,14 @@ let question2: Question = {
     "None of these",
   ],
   correctAnsIndex: 2,
-};
-questions.push(question2);
+});
 
 // q 3
-let question3: Question = {
-  question: "What is the output of console.log( 2 + '3' ) ?",
+questions.push({
+  question: "Q no 3 ) What is the output of console.log( 2 + '3' ) ?",
   options: ["5", "23", "Error", "None of these"],
   correctAnsIndex: 0,
-};
-questions.push(question3);
+});
 
 // console.log(questions);
 // I add 3 questions, you can add more as you want...
@@ -48,3 +45,31 @@ questions.push(question3);
 console.log("-".repeat(50));
 console.log("\tWelcome in Typescript Quizz...");
 console.log("-".repeat(50));
+
+let result = 0;
+
+for (let i = 0; i < questions.length; i++) {
+  console.log(questions[i].question);
+  let answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "option",
+      choices: questions[i].options,
+      message: "Select an option : ",
+    },
+  ]);
+  if (answer.option === questions[i].options[questions[i].correctAnsIndex]) {
+    result++;
+    console.log("Correct");
+  } else {
+    console.log(
+      "\n\tIncorrect answer! correct answer is " +
+        questions[i].options[questions[i].correctAnsIndex] +
+        "\n"
+    );
+  }
+}
+
+console.log(`You give ${result} out of ${questions.length} correct answers.`);
+let percentage = (result / questions.length) * 100;
+console.log(`Your percentage is ${percentage}`);
